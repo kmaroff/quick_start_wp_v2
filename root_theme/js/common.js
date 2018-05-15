628717fcae748270d48a7c4ed2301716ec56ed26
kmaroff <artem.komar@gmail.com>
2018-05-15 22:15:57 +0400

    commit 2

--- root_theme/js/common.js
+++ root_theme/js/common.js
@@ -5,110 +5,21 @@ $(function() {
     $(".burger-menu").toggleClass("menu-on");
     $(".burger-menu-text").toggleClass("is-open");
   });
- 
-  $(window).resize(function(){
-  var w = $(window).width();
-  menu    = $('.main-nav--mobile');
-  if(w > 480 && menu.is(':hidden')) {
-    menu.removeAttr('style');
-  }
-});
 
-particlesJS("particles-js", {
-  "particles": {
-    "number": {
-      "value": 100,
-      "density": {
-        "enable": true,
-        "value_area":1000
-      }
-    },
-    "color": {
-      "value": ["#e3e4e4", "#e3e4e4", "#e3e4e4", "#e3e4e4"]
-    },
-    
-    "shape": {
-      "type": "circle",
-      "stroke": {
-        "width": 0,
-        "color": "#bdbdbd"
-      },
-      "polygon": {
-        "nb_sides": 5
-      },
-      "image": {
-        "src": "img/github.svg",
-        "width": 100,
-        "height": 100
-      }
-    },
-    "opacity": {
-      "value": 0.6,
-      "random": false,
-      "anim": {
-        "enable": false,
-        "speed": 1,
-        "opacity_min": 0.1,
-        "sync": false
-      }
-    },
-    "size": {
-      "value": 15,
-      "random": true,
-      "anim": {
-        "enable": false,
-        "speed": 40,
-        "size_min": 0.1,
-        "sync": false
-      }
-    },
-    "line_linked": {
-      "enable": true,
-      "distance": 120,
-      "color": "#bdbdbd",
-      "opacity": 0.4,
-      "width": 1
-    },
-  },
-  "interactivity": {
-    "detect_on": "canvas",
-    "events": {
-      "onhover": {
-        "enable": true,
-        "mode": "grab"
-      },
-      "onclick": {
-        "enable": false
-      },
-      "resize": true
-    },
-    "modes": {
-      "grab": {
-        "distance": 140,
-        "line_linked": {
-          "opacity": 1
-        }
-      },
-      "bubble": {
-        "distance": 400,
-        "size": 40,
-        "duration": 2,
-        "opacity": 8,
-        "speed": 3
-      },
-      "repulse": {
-        "distance": 200,
-        "duration": 0.4
-      },
-      "push": {
-        "particles_nb": 4
-      },
-      "remove": {
-        "particles_nb": 2
-      }
+
+  $(window).resize(function(){
+    var w = $(window).width();
+    menu    = $('.main-nav--mobile');
+    if(w > 480 && menu.is(':hidden')) {
+      menu.removeAttr('style');
     }
-  },
-  "retina_detect": true
-});
+  });
+
+  // sticky header menu
+  $(window).scroll(function() {
+    var top = $(document).scrollTop();
+    if (top < 300) $(".top_nav").removeClass('fixed');
+    else $(".top_nav").addClass('fixed');
+  });
 
 });
